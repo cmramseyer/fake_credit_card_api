edcfdc5dacdd3cf0f7a39cf9ed5aa3f17d4e1cea
Carlos Ramseyer <carlos@bytelion.com>
2019-09-19 16:50:30 -0300

    added basic auth with fix token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+
+  def authenticate_user!
+    return true if request.headers["authorization"] =="Bearer #{ENV['CREDIT_CARD_API_TOKEN']}"
+    false
+  end
+
 end
---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,6 @@
+class CreditCardsController < ApplicationController
+  def index
+    @credit_cards = CreditCard.all
+    render json: @credit_cards
+  end
+end
