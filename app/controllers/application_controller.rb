1499ab9e126571f328bc2f2bdf64a5bbad668d2a
Carlos Ramseyer <carlos@bytelion.com>
2019-09-23 23:17:03 -0300

    added rescue_from actions in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ArgumentError, with: :argument_error
+
   def authenticate_user!
     return true if request.headers["authorization"] =="Bearer #{ENV['CREDIT_CARD_API_TOKEN']}"
     false
+  end
+
+  def record_not_found(exception)
+    # custom code for record not found
+    # ...
+    render json: { message: exception.message }, status: 500
+  end
+
+  def argument_error(exception)
+    # custom code for argument error
+    # ...
+    render json: { message: exception.message }, status: 500
   end
 
 end
